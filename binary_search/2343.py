5e02712e9b1271b53e7212b4087b6eb7e7b16a3e
SungKyum Kim <sungkyum1@naver.com>
2024-01-24 22:49:16 +0900

    문제 제목 수정

--- binary_search/2343.py
+++ binary_search/2343.py
@@ -1,28 +1,16 @@
 from sys import stdin as reader
 
-n,m = map(int,reader.readline().split())
-trees = list(map(int, reader.readline().split()))
+def binary_search(files, target):
 
-start, end = 0, max(trees)
+
+
+n,m = map(int,reader.readline().split())
+files = list(map(int, reader.readline().split()))
+
+start, end = min(files), max(files)
 mid = (start + end) // 2
 
-result = 0
-while start <= end: 
-    length = 0
-    for tree in trees :
-        if tree > mid: 
-            length += tree - mid
+while start<=end: 
     
-    if length > m : 
-        start = mid + 1
-        mid = (start + end) // 2
-    elif length < m : 
-        end = mid - 1 
-        mid = (start + end) // 2
-    else : 
-        result = mid
-        break
-
-    result = mid
 
 print (result)
